be74046a235be8e88a96299bb96fac1b6d9c463d
Legodingo13 <Legodingo13@gmail.com>
2026-09-19 17:29:19 +0200

    Update admin-config.js

--- admin-config.js
+++ admin-config.js
@@ -1,2 +1,7 @@
-// Fichier à remplacer par celui généré avec OUTIL_LOCAL_NE_PAS_METTRE_SUR_LE_SITE/creer_admin_config.html
-window.LEGODINGO13_ADMIN_CONFIG = null;
+window.LEGODINGO13_ADMIN_CONFIG = {
+  "version": 1,
+  "iterations": 310000,
+  "salt": "ZevDMYh3vHzKiWxpvWGx2A==",
+  "iv": "llIlIIwiop1WOzIn",
+  "ciphertext": "MzrdcyyBQ3m/mVXrdzTWq908b3iVVElHQI6WKzq2P4+q59UNYgTvxdCyaqcI37sIMeWimKSPkNRpOlQsAlrW5Db366HZhLhyX2kCAxLDrXK1r/Q6tlqZm73IC1yDdUzbl3CNz1kZTe4fTyu/Dw=="
+};
